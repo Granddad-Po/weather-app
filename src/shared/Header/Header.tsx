7ab2750702d1db362487c5991808066197c1c1e1
Vitaliy Po <76914279+Granddad-Po@users.noreply.github.com>
2022-04-12 21:52:01 +0300

    add: local storage and fix change theme

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GlobalSvgSelector } from "../../assets/icons/global/GlobalSvgSelector";
 import Select, { NonceProvider } from 'react-select'
 
 import s from './Header.module.scss';
 import SingleValue from "react-select/dist/declarations/src/components/SingleValue";
+import { useTheme } from "../../hooks/useTheme";
+import { Theme } from "../../context/ThemeContext";
 
 interface Props {
 
 }
 
 export const Header = (props: Props) => {
+const theme = useTheme();
+
 	const options = [
 		{ value: 'spb', label: 'Saint-Petersburg' },
 		{ value: 'moscow', label: 'Moscow' },
@@ -19,7 +23,7 @@ export const Header = (props: Props) => {
 	const colourStyles = {
 		control: (styles: any) => ({
 			...styles,
-			backgroundColor: 0 ? '#4F4F4F' : 'rgba(71, 147, 255, 0.2)',
+			backgroundColor: theme.theme === Theme.DARK ? '#4F4F4F' : 'rgba(71, 147, 255, 0.2)',
 			width: '194px',
 			height: '37px',
 			border: 'none',
@@ -28,10 +32,13 @@ export const Header = (props: Props) => {
 		}),
 		singleValue: (styles: any) => ({
 			...styles,
-			color: 0 ? '#fff' : '#000',
+			color: theme.theme === Theme.DARK ? '#fff' : '#000',
 		})
 	}
 
+	function changeTheme() {
+		theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+	}
 
 	return (
 		<header className={s.header}>
@@ -42,7 +49,7 @@ export const Header = (props: Props) => {
 				<h2 className={s.header__title}>React weather</h2>
 			</div>
 			<div className={s.wrapper}>
-				<div className={s.change_theme}>
+				<div className={s.change_theme} onClick={changeTheme}>
 					<GlobalSvgSelector id='change-theme' />
 				</div>
 				<Select defaultValue={options[0]} styles={colourStyles} options={options} />
